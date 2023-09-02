bee8a7373e5e1c84cdb297c6dcfc8808ef076e16
EunchanJeong <jec65147@gmail.com>
2023-09-02 18:03:24 +0900

    update : 친구 만들기 - 사용자 입력

--- config/setting_persona/forms.py
+++ config/setting_persona/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Friend
+from .models import Friend, User
 
 class FriendForm(forms.ModelForm):
     canvas_image = forms.CharField(widget=forms.HiddenInput)
@@ -32,3 +32,25 @@ class FriendForm(forms.ModelForm):
     class Meta:
         model = Friend
         fields = ('character_name', 'age', 'gender', 'likes', 'dislikes')
+        
+class UserForm(forms.ModelForm):
+
+    user_name = forms.CharField(
+        label="나의 이름",
+        widget=forms.TextInput(attrs={"class": "form-control form-control-lg"}),
+        required=True
+    )
+    user_age = forms.ChoiceField(
+        label="나의 나이",
+        widget=forms.Select(attrs={"class": "form-select form-select-lg"}),
+        choices=[(str(i), str(i)) for i in range(5, 11)],
+    )
+    user_gender = forms.ChoiceField(
+        label="나의 성별",
+        widget=forms.Select(attrs={"class": "form-select form-select-lg"}),
+        choices=[("male", "남"), ("female", "여")],
+    )
+
+    class Meta:
+        model = User
+        fields = ('user_name', 'user_age', 'user_gender',)
